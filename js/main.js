5f24eb72774dcfa714bc72bebfbfb56a77e5369c
Sabry-63 <52372174+Sabry-63@users.noreply.github.com>
2019-07-05 01:52:36 +0200

    LooLab

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// wow func
+new WOW().init();
+function activateAnimate()
+{
+    new WOW().init();
+}
+//------
+
+$(function(){
+    //scroll smosh
+    $('.nav-link').click(function(){
+        $('html, body').animate({
+            scrollTop: $('#' + $(this).data('value')).offset().top + 1
+        }, 1000);
+    });
+
+    //active
+    $('.nav-link').click(function(){
+        $(this).parent().siblings().children().removeClass('active');
+        $(this).addClass('active');
+    });
+
+
+
+});
